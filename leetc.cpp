d7427e9f16f5116bd9f714bd4bc0007faea434ba
Soham0112 <soham011203@gmail.com>
2024-07-08 14:35:16 +0530

    remove duplicates but too much runtime(12ms)

--- leetc.cpp
+++ leetc.cpp
@@ -1,115 +1,71 @@
 #include <iostream>
+#include<vector>
 
 using namespace std;
-
-struct ListNode 
+class Solution 
 {
-  int val;
-  ListNode *next;
-  ListNode() : val(0), next(nullptr) {}
-  ListNode(int x) : val(x), next(nullptr) {}
-  ListNode(int x, ListNode *next) : val(x), next(next) {}
+    public:
+        
+        int removeDuplicates(vector<int>& nums) 
+        {
+            if(nums.empty())
+            {
+                return 0;
+            }
+            vector<int> temp={nums[0]};
+            for(int i =0; i<nums.size()-1;i++)
+            {
+
+                if(nums[i]==nums[i+1])
+                {
+                    continue;
+                }
+                else
+                {
+                    temp.push_back(nums[i+1]);
+                }
+
+            }
+
+            nums.clear();
+            for(int i=0; i<=temp.size();i++)
+            {
+                nums.push_back(temp[i]);
+                cout<<temp[i]<<" ";
+            }
+            cout<<endl;
+            for(int i=0; i<=nums.size();i++)
+            {
+                cout<<nums[i]<<" ";
+            }
+            int k = temp.size();
+
+            nums.shrink_to_fit();
+
+
+            return k;
+        }
 };
 
 
-class Solution {
-public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) 
-    {
-        
 
-       
-        if(list1 == nullptr)
-        {
-            return list2;
-        }
-
-        else if(list2 == nullptr)
-        {
-            return list1;
-        }
-
-        ListNode* list3= new ListNode();
-        ListNode* tail=list3;
-
-        while(list1 !=nullptr && list2 !=nullptr)
-
-        {
-            if(list1->val <= list2->val)
-            {
-                tail->next = list1;
-                list1 = list1->next;
-            }
-
-            else
-            {
-                tail->next = list2;
-                list2 = list2->next;
-            }
-
-            tail=tail->next;
-        }
-
-        if(list1 == nullptr)
-        {
-            tail->next = list1;
-        }
-        else
-        {
-            tail->next = list2;
-        }
-        
-
-        return list3->next;
-    }
-
-};
 
 int main() 
 {
-    ListNode* list1_head = new ListNode(5);
-    list1_head->next = new ListNode(15);
-    list1_head->next->next = new ListNode(25);
+    vector<int> nums={1,1,1,2,2,3,3,3};
 
-    
-    ListNode* list2_head = new ListNode(3);
-    list2_head->next = new ListNode(13);
-    list2_head->next->next = new ListNode(23);
-
-    Solution list3;
-
-    ListNode * ans=list3.mergeTwoLists(list1_head,list2_head);
-
-    int count = 0;
-    ListNode* temp = ans;
-    while (temp != nullptr) 
+    for(int i=0; i <= nums.size();i++)
     {
-        count++;
-        temp = temp->next;
+        cout<<nums[i]<<endl;
     }
 
-    // Create an array to store the merged elements
-    int merged_array[count];
+    Solution solu_vec;
+    int k = solu_vec.removeDuplicates(nums);
 
-     // Iterate through the merged list and populate the array
-    int i = 0;
-    temp = ans;
-    while (temp != nullptr) 
-    {
-        merged_array[i++] = temp->val;
-        temp = temp->next;
-    }
+    cout<<k<<endl;
 
-  // Print the merged elements in the array
-    for (int i = 0; i < count; i++) 
-    {
-        cout << merged_array[i] << " ";
-    }
-    cout << endl;
 
     return 0;
-
-
-
 }
+
 

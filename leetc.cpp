45fec5fe65216e2d6edbbff540660fd7ea134020
Soham0112 <soham011203@gmail.com>
2024-07-10 00:58:49 +0530

    plus one done

--- leetc.cpp
+++ leetc.cpp
@@ -4,48 +4,46 @@
 
 using namespace std;
 
-class Solution {
-public:
-    vector<int> plusOne(vector<int>& digits) 
-    {
-        long long int num=0;
-        string str;
-        switch (digits[digits.size()-1])
-        {
-        case 9:
-            for(int i=digits.size()-1; i>=0 ;i--)
-            {
-                num += digits[i] * (pow(10,(digits.size()-1)-i));
-            }
-            num += 1;
-            str = to_string(num);
-            digits.clear();
-            for (char c : str) 
-            {
-                digits.push_back(c - '0');
-            }
-            return digits;
-        
-        default:
-            cout<<"default is working";
-            digits[digits.size()-1]+=1;
-        }
 
-        return digits;
-        
-    }
-};
 
 int main() 
 {
-    vector<int> digits ={9};
-
-    Solution vec_dig;
-    digits=vec_dig.plusOne(digits);
-
-    for(int i=0; i < digits.size();i++)
+    vector<int> digits ={9,9,9};
+    if(digits[digits.size()-1]<9)
     {
-        cout<<digits[i]<<' ';
+        digits[digits.size()-1] +=1;
+        for(int i =0; i <digits.size();i++)
+        {
+            cout<<"the number is:"<<digits[i]<<endl;
+        }
+        return 0;
+    }
+    else
+    {
+        for(int i=digits.size()-1; i>=0;i--)
+        {
+            if(digits[i]==9)
+            {
+                digits[i]=0;
+            }
+            else
+            {
+                digits[i] +=1;
+                for(int i =0; i <digits.size();i++)
+                {
+                    cout<<"the number is:"<<digits[i]<<endl;
+                }
+                return 0;
+            }
+
+        }
+        digits.insert(digits.begin(),1);
+    }
+
+    for(int i =0; i <digits.size();i++)
+    {
+        cout<<"the number is:"<<digits[i]<<endl;
     }
     return 0;
 }
+

f289eb6bf2721c11598f6b4e4794ecfce1bed6b0
Soham0112 <soham011203@gmail.com>
2024-07-11 01:09:03 +0530

    added binary number but stings .runtime 6ms

--- leetc.cpp
+++ leetc.cpp
@@ -1,49 +1,111 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
+#include <string>
 
 using namespace std;
 
+class Solution {
+public:
+    string equal_add(string &a ,string &b)
+    {
+        int carry=0;
+        for(int i=b.size()-1; i>-1 ;i--)
+        {
+            if(a[i] == '1' && b[i] == '1')
+            {
+                if(carry != 0)
+                {
+                    b[i]='1';
+                    carry =1;
+                }
+                else
+                {
+                    b[i]='0';
+                    carry =1;
+                }
+            }
 
+            else if((a[i] == '1' && b[i] == '0') || (a[i] == '0' && b[i] == '1'))
+            {
+                if(carry != 0)
+                {
+                    b[i]='0';
+                    carry =1;
+                }
+                else
+                {
+                    b[i] ='1';
+                }
+            }
+
+            else
+            {
+                if(carry != 0)
+                {
+                    b[i]='1';
+                    carry -=1;
+                }
+                else
+                {
+                    b[i]='0';
+                }
+            }
+        }
+        if(carry==1)
+        {
+            cout<<"with carry"<<endl;
+            string ans1 = '1'+ b;
+            return ans1;
+        }
+        else
+        {
+            cout<<"no carry"<<endl;
+            return b;
+        }
+
+
+        return b;
+
+    }
+    string addBinary(string &a, string &b) 
+    {
+        if (a.size() < b.size())
+        {
+            int diff=b.size()-a.size();
+            for(int i=0; i<diff; i++)
+            {
+                a.insert(a.begin(),'0');
+            }
+            return equal_add(b, a);
+        }
+        else if (a.size() > b.size())
+        {
+            int diff=a.size()-b.size();
+            for(int i=0; i<diff; i++)
+            {
+                b.insert(b.begin(),'0');
+            }
+            return equal_add(a, b);
+        }
+        else
+        {
+            return equal_add(a, b);
+        }
+    }
+};
 
 int main() 
 {
-    vector<int> digits ={9,9,9};
-    if(digits[digits.size()-1]<9)
-    {
-        digits[digits.size()-1] +=1;
-        for(int i =0; i <digits.size();i++)
-        {
-            cout<<"the number is:"<<digits[i]<<endl;
-        }
-        return 0;
-    }
-    else
-    {
-        for(int i=digits.size()-1; i>=0;i--)
-        {
-            if(digits[i]==9)
-            {
-                digits[i]=0;
-            }
-            else
-            {
-                digits[i] +=1;
-                for(int i =0; i <digits.size();i++)
-                {
-                    cout<<"the number is:"<<digits[i]<<endl;
-                }
-                return 0;
-            }
+    string a={"1"};
+    string b={"11"};
+    
+    Solution str;
+    string ans= str.addBinary(a,b);
 
-        }
-        digits.insert(digits.begin(),1);
+    for(int i=0;i<ans.size();i++)
+    {
+        cout<<ans[i]<<" ";
     }
 
-    for(int i =0; i <digits.size();i++)
-    {
-        cout<<"the number is:"<<digits[i]<<endl;
-    }
-    return 0;
+    
 }
-

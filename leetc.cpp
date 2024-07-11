3affe65f252bf895193448851d7d9d20bc01ad12
Soham0112 <soham011203@gmail.com>
2024-07-11 18:40:20 +0530

    binary search for sqrt. 10ms runtime

--- leetc.cpp
+++ leetc.cpp
@@ -4,108 +4,38 @@
 
 using namespace std;
 
-class Solution {
-public:
-    string equal_add(string &a ,string &b)
+
+int square_root(int x,int low,int high)
+{
+    if (x == 1)
     {
-        int carry=0;
-        for(int i=b.size()-1; i>-1 ;i--)
-        {
-            if(a[i] == '1' && b[i] == '1')
-            {
-                if(carry != 0)
-                {
-                    b[i]='1';
-                    carry =1;
-                }
-                else
-                {
-                    b[i]='0';
-                    carry =1;
-                }
-            }
-
-            else if((a[i] == '1' && b[i] == '0') || (a[i] == '0' && b[i] == '1'))
-            {
-                if(carry != 0)
-                {
-                    b[i]='0';
-                    carry =1;
-                }
-                else
-                {
-                    b[i] ='1';
-                }
-            }
-
-            else
-            {
-                if(carry != 0)
-                {
-                    b[i]='1';
-                    carry -=1;
-                }
-                else
-                {
-                    b[i]='0';
-                }
-            }
-        }
-        if(carry==1)
-        {
-            cout<<"with carry"<<endl;
-            string ans1 = '1'+ b;
-            return ans1;
-        }
-        else
-        {
-            cout<<"no carry"<<endl;
-            return b;
-        }
-
-
-        return b;
-
+        return 1;
     }
-    string addBinary(string &a, string &b) 
+    int mid = low+(high - low) / 2;
+    if (mid * mid == x)
     {
-        if (a.size() < b.size())
-        {
-            int diff=b.size()-a.size();
-            for(int i=0; i<diff; i++)
-            {
-                a.insert(a.begin(),'0');
-            }
-            return equal_add(b, a);
-        }
-        else if (a.size() > b.size())
-        {
-            int diff=a.size()-b.size();
-            for(int i=0; i<diff; i++)
-            {
-                b.insert(b.begin(),'0');
-            }
-            return equal_add(a, b);
-        }
-        else
-        {
-            return equal_add(a, b);
-        }
+        return mid; 
     }
-};
+    else if ((mid + 1) * (mid + 1) > x && (mid - 1) * (mid - 1) < x)
+    {
+        return mid-1; 
+    }
+    else if (mid * mid < x)
+    {
+        cout<<x<<" "<<mid<<" "<<high<<endl;
+        return square_root(x, mid, high);
+    }
+    else
+    {
+        return square_root(x, low, mid);
+    }
+}
+
 
 int main() 
 {
-    string a={"1"};
-    string b={"11"};
-    
-    Solution str;
-    string ans= str.addBinary(a,b);
+    int sqr_rt=square_root(25,0,25);
+    cout<<"sqaure root is "<<sqr_rt<<endl;
+    return 0;
 
-    for(int i=0;i<ans.size();i++)
-    {
-        cout<<ans[i]<<" ";
-    }
-
-    
 }

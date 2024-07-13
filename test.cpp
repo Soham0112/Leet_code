7ca290e5711e27fbcbeaf75f5b83c1a99651f949
Soham0112 <soham011203@gmail.com>
2024-07-13 16:24:55 +0530

    recursion tower of hanoi.

--- test.cpp
+++ test.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
-#include <string>
+#include <vector>
+
 using namespace std;
+
+int tower_hanoi(int x)
+{
+    switch (x)
+    {
+    case 1:
+        return 1;
+
+    default:
+        return 2 * tower_hanoi(x - 1) + 1;
+    }
+}
 int main()
 {
-    int x=4;
-    int *ptr,**ptr;
-    ptr=&x;
+    int x;
 
-    cout<<x<<endl;
-    cout<<&x<<endl;
-    cout<<**ptr<<endl;
-    cout<<*ptr<<endl;   // *ptr is basically the value of x
-    cout<<ptr<<endl;    // ptr is basically the address of x
-    cout<<&ptr<<endl;   // &ptr is basically address of ptr
-    cout<<&*ptr<<endl;  // & *ptr = & x basically address of x
-    cout<<*&ptr<<endl;  // * &ptr is value at address ptr i.e address of x
-    cout<<"end of results"<<endl;
+    cout << "enter the number of disks" << endl;
+    cin >> x;
 
+    int y = tower_hanoi(x);
+    cout << "the number of steps is" << y << endl;
     return 0;
-}   
-
+}

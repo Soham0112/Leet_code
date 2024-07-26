58084cdc18da141819c73d9f6e3913fa56a8a134
Soham0112 <soham011203@gmail.com>
2024-07-26 12:16:17 +0530

    solved using iteration

--- leetcM2.cpp
+++ leetcM2.cpp
@@ -18,41 +18,79 @@ void insertAtTail(ListNode* &head, int value)
         head = newNode;
         return;
     }
+
+    ListNode* temp = head;
+    while(temp->next != nullptr)
+    {
+        temp=temp->next;
+    }
+
+    temp->next=newNode;
 }
 
 
 
 ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) 
 {
-    ListNode* l3 = new ListNode();
+    ListNode* l3 = new ListNode(-1);
+    ListNode* l3p = l3;
 
     int carry=0;
 
-    while(l1 != nullptr || l2 != nullptr)
+    while(l1 != nullptr && l2 != nullptr)
     {
         cout<<"this is working"<<endl;
-        int temp_sum= (l1->val + l2->val) % 10;
-        insertAtTail(l3, (temp_sum+carry));
+        int temp_sum= (l1->val + l2->val+carry) % 10;
+        insertAtTail(l3, (temp_sum));
 
-        carry = (l1->val + l2->val) / 10;
+        carry = (l1->val + l2->val+carry) / 10;
         l1=l1->next;
         l2=l2->next;
     }
 
+    while(l1 != nullptr)
+    {
+        cout<<"this is working 1"<<endl;
+        int temp_sum= (l1->val+carry) % 10;
+        insertAtTail(l3, (temp_sum));
+
+        carry = (l1->val+carry) / 10;
+        l1=l1->next;
+    }
+
+    while(l2 != nullptr)
+    {
+        cout<<"this is working 2"<<endl;
+        int temp_sum= (l2->val+carry) % 10;
+        insertAtTail(l3, (temp_sum));
+
+        carry = (l2->val+carry) / 10;
+        l2=l2->next;
+    }
+
+    if(carry>0)
+    {
+        insertAtTail(l3,carry);
+    }
 
 
-    return l3;
+
+    return l3->next;
 }
 
 
 int main()
 {
-    ListNode *head1 = new ListNode(1);                     
-    head1->next = new ListNode(2, new ListNode(3, nullptr)); 
+    ListNode *head1 = new ListNode(3);                     
+    insertAtTail(head1,7);
+    // insertAtTail(head1,9);
 
 
-    ListNode *head2 = new ListNode(4);
-    head2->next = new ListNode(5, new ListNode(6, nullptr));
+    ListNode *head2 = new ListNode(9);
+    insertAtTail(head2,2);
+    // insertAtTail(head2,9);
+    // insertAtTail(head2,9);
+    // insertAtTail(head2,9);
 
 
     ListNode *current = head1;

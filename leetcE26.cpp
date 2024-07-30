3497b560275a6f29bedd32d8748b428c288c61b8
Soham0112 <soham011203@gmail.com>
2024-07-30 07:27:25 +0530

    anagram using hash and sort

---
+++ leetcE26.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isAnagram(string s, string t) 
+{
+    sort(s.begin(),s.end());
+    sort(t.begin(),t.end());
+
+    unordered_map<string,int> anagram;
+
+    anagram[s]=1;
+    if(anagram.find(t) != anagram.end())
+    {
+        return true;
+    }
+
+    return false;
+
+}
+
+
+int main()
+{
+    string s="anagram";
+    string t="naamgrt";
+
+    cout<<isAnagram(s,t)<<endl;
+    return 0;
+}

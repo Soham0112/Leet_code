905c39ae5774ec2c1d1777631b1612e87c1287dd
Soham0112 <soham011203@gmail.com>
2024-08-01 18:06:50 +0530

    complete day direct

--- leetcE27.cpp
+++ leetcE27.cpp
@@ -3,34 +3,14 @@ using namespace std;
 
 int countCompleteDayPairs(vector<int>& hours) 
 {
-    map<int,int> hours_map;
-
-    for(int i=0 ; i < hours.size(); i++)
-    {
-        hours_map[i] = hours[i];
-    }
-
-    if(hours_map.empty())
-    {
-        return 0;
-    }
-
-    for (auto it = hours_map.begin(); it != hours_map.end(); ++it) 
-    {
-        cout << it->first << " : " << it->second <<endl;
-    }
-
     int count=0;
 
     for(int i= 0 ; i < hours.size()-1 ; i++)
     {
         for(int j=i+1 ; j < hours.size(); j++)
         {
-            if((hours_map[i]+hours_map[j])%24 == 0)
+            if((hours[i]+hours[j])%24 == 0)
             {
-                cout<<i<<" "<<j<<endl;
-                cout<<hours_map[i]<<" "<<hours_map[j]<<endl;
-                cout<<endl;
                 count++;
             }
         }

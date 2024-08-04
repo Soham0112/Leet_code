34239cd407e75166de340c660d14f44c7c820d4e
Soham0112 <soham011203@gmail.com>
2024-08-04 10:15:39 +0530

    vector index access is faster than map key access.

--- leetcE32.cpp
+++ leetcE32.cpp
@@ -3,17 +3,17 @@ using namespace std;
 
 int firstUniqChar(string s) 
 {
-    unordered_map<char,int> count_map;
+    vector<int> count_vec(26,0);
     int s_len = s.length();
 
     for(int i=0 ; i < s_len; i++)
     {
-        count_map[s[i]]++;
+        count_vec[s[i]-'a']++;
     }
 
     for(int i=0 ; i < s_len; i++)
     {
-        if(count_map[s[i]] == 1)
+        if(count_vec[s[i]-'a'] == 1)
         {
             cout<<s[i]<<endl;
             return i;
